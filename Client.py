64202f435c5dcbbb6222ae4a56f75a87024a9204
Hiang Kwui Ng <stslxg@gmail.com>
2019-11-07 16:26:55 -0500

    change Client

--- Client.py
+++ Client.py
@@ -8,17 +8,19 @@ class Client:
         return int + 75
     
     def find_leader(self):
-        return leader
+        return self.leader
 
     def put(self, key, value):
-        # send put message to leader
-        # if...
-            # response times out
-                # increment leader index and loop
-            # response is false
-                # try again
-            # response is success
-                # return 
+        """
+        send put message to leader
+        if...
+            response times out
+                increment leader index and loop
+            response is false
+                try again
+            response is success
+                return 
+        """
         return None
 
     def get (self, key):
