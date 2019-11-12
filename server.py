1c50644d60b1cfa95d8942b794bb5edbcdb15ccd
Hiang Kwui Ng <stslxg@gmail.com>
2019-11-12 01:05:28 -0500

    .

--- server.py
+++ server.py
@@ -5,20 +5,20 @@ from config import Config
 from connection import ServerConnection, ClientConnection
 
 class Server:
-    __slots__ = ["_conn", "_my_id", "_server_num"]
+    __slots__ = ["_conn", "_my_id", "_server_num", "_loop"]
 
     def __init__(self, config=Config):
         self._conn = ServerConnection(config)
         self._my_id = self._conn.server_id
         self._server_num = len(config.SERVER_NAMES)
+        self._loop = asyncio.get_event_loop()
 
     async def server_handler(self):
         print(self._my_id)
         msg2 = str(self._my_id)
         while True:
-            for id in iter(range(self._server_num)):
-                if id != self._my_id:
-                    await self._conn.send_data_to_server(msg2.encode(), id)
+            await asyncio.gather([self._conn.send_data_to_server(msg2.encode(), id)
+                for id in iter(range(self._server_num)) if id != self._my_id])
             await asyncio.sleep(1)
             data = await self._conn.receive_data_from_server()
             msg = data.decode()
@@ -28,12 +28,11 @@ class Server:
     async def client_handler(self):
         while True:
             await asyncio.sleep(5)
-            print('...')
+            print('....................')
 
     def run(self):
         with self._conn:
-            loop = asyncio.get_event_loop()
-            loop.run_until_complete(asyncio.gather(Server.server_handler(self), Server.client_handler(self)))
+            self._loop.run_until_complete(asyncio.gather(Server.server_handler(self), Server.client_handler(self)))
 
 if __name__ == "__main__":
     server = Server()
