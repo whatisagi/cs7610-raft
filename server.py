2d9c496b4795025386b4ea8ed479c4d1b4d9abfe
Hiang Kwui Ng <stslxg@gmail.com>
2019-11-12 03:12:00 -0500

    testing double dispatch

--- server.py
+++ server.py
@@ -6,27 +6,27 @@ from connection import ServerConnection
 from messages import *
 
 class Server:
-    __slots__ = ["_conn", "_my_id", "_server_num", "_loop"]
+    __slots__ = ["_conn", "_id", "_server_num", "_loop"]
 
     def __init__(self, config=Config):
         self._conn = ServerConnection(config)
-        self._my_id = self._conn.server_id
+        self._id = self._conn.server_id
         self._server_num = len(config.SERVER_NAMES)
         self._loop = asyncio.get_event_loop()
 
     def test_handle(self, msg):
-        print(msg)
+        print(self._id, ':', msg)
 
     async def server_handler(self):
-        print("I'm Server", self._my_id)
-        msg = Test(self._my_id)
+        print("I'm Server", self._id)
+        msg = Test(self._id)
         while True:
             await asyncio.gather(*(self._conn.send_message_to_server(msg, id)
-                for id in range(self._server_num) if id != self._my_id))
+                for id in range(self._server_num) if id != self._id))
             await asyncio.sleep(1)
             msg = await self._conn.receive_message_from_server()
             msg.handle(self)
-            msg = Test(self._my_id)
+            msg = Test(self._id)
 
     async def client_handler(self):
         while True:
