4fe95fd0591a390ce9e4207abf33002b8aea1269
Hiang Kwui Ng <stslxg@gmail.com>
2019-11-12 00:49:51 -0500

    testing

--- server.py
+++ server.py
@@ -13,17 +13,16 @@ class Server:
         self._server_num = len(config.SERVER_NAMES)
 
     async def server_handler(self):
-        if self._my_id == 0:
-            data = u"1".encode()
-            await self._conn.send_data_to_server(data, 1)
+        print(self._my_id)
+        msg2 = str(self._my_id)
         while True:
+            async for id in iter(range(self._server_num)):
+                if id != self._my_id:
+                    await self._conn.send_data_to_server(msg2.encode(), id)
             data = await self._conn.receive_data_from_server()
             msg = data.decode()
             print(msg)
             msg2 = msg + u"1"
-            async for id in iter(range(self._server_num)):
-                if id != self._my_id:
-                    await self._conn.send_data_to_server(msg2.encode(), id)
 
     async def client_handler(self):
         while True:
