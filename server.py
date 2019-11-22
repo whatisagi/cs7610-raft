140c4265e41da3c7722f4737ab614fd6668d810a
Hiang Kwui Ng <stslxg@gmail.com>
2019-11-22 15:06:41 -0500

    send heartbeats to new config

--- server.py
+++ server.py
@@ -244,6 +244,9 @@ class Server:
                 reply_msg = AddServersReply(msg.messageId, False, self._id, True, self.serverConfig)
             else:
                 new_config = self.serverConfig.union(msg.servers)
+                for id in range(self._server_num):
+                    if id != self._id and id in self.serverNewConfig and id not in self.serverConfig:
+                        self._heartbeat_timer.append(self._loop.create_task(self.heartbeat_sender(id)))
                 op = ConfigOp(self.currentTerm, self.serverConfig, new_config)
                 print("Starting joint consensus {}".format(op))
                 commit_success = await self.op_handler(op)
