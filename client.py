f397a1c32c5c16bf719ddaf10289d84d125c3634
David Stalfa <david.stalfa@gmail.com>
2019-11-24 14:59:56 -0500

    david: adding servers to config after config change

--- client.py
+++ client.py
@@ -7,21 +7,23 @@ import queue
 import asyncio
 import sys
 import random
+from typing import Optional, List, Dict, Tuple, Set
 from contextlib import suppress
 from config import Config
 from connection import ClientConnection
 from messages import *
 
-DELAY = 3               # client timeout delay (in seconds)
-CHOOSE_RANDOM_LEADER = True     # flag telling client to randomly choose new leader
+DELAY = 3                   # client timeout delay (in seconds)
+CHOOSE_RANDOM_LEADER = True # flag telling client to randomly choose new leader
 
 SEND_NEW = True         # flag telling client to send a new instruction
 SEND_SUCCESS = False    # flag telling client that last message was received by leader
 
-ADD_NEW = True  # flag telling client to add new servers
+ADD_NEW = True      # flag telling client to add new servers
 ADD_SUCCESS = False # flag telling client that last requested servers were added
+ADD_DELAY = 6
 
-ALL_SENT_MESSAGES = {} # dictionary to store all messages sent keyed by message ID
+ALL_SENT_MESSAGES = {}  # dictionary to store all sent messages, keyed by message ID
 
 
 class Client:
@@ -64,15 +66,12 @@ class Client:
         global CHOOSE_RANDOM_LEADER
         global ALL_SENT_MESSAGES
         if CHOOSE_RANDOM_LEADER:
-            #print("choosing random leader (get/put)") #debug
             self.leader = random.choice(list(self.config.INIT_SERVER_CONFIG))
         CHOOSE_RANDOM_LEADER = True
-        #print("sending get/put to leader: ", self.leader) #debug
+        print("sending get/put message to leader: ", self.leader) #debug
         self.msgId = self.msgId + 1
         instruction.messageId = self.msgId
         ALL_SENT_MESSAGES[self.msgId] = instruction
-        #print("message", ALL_SENT_MESSAGES[self.msgId].messageId, "stored in dictionary at index", self.msgId) #debug
-        #await instruction.handle(self) #debug
         await self._conn.send_message_to_server(instruction, self.leader)
 
     async def tryAdd(self):
@@ -88,7 +87,7 @@ class Client:
                 await self.sendAddServers(msg_to_server)
                 ADD_NEW = False
             if not ADD_NEW:
-                await asyncio.sleep(DELAY)
+                await asyncio.sleep(ADD_DELAY)
                 if not ADD_SUCCESS:
                     await self.sendAddServers(msg_to_server)
             if ADD_SUCCESS:
@@ -99,7 +98,6 @@ class Client:
         global CHOOSE_RANDOM_LEADER
         global ALL_SENT_MESSAGES
         if CHOOSE_RANDOM_LEADER:
-            #print("choosing random leader (add)") #debug
             self.leader = random.choice(list(self.config.INIT_SERVER_CONFIG))
         CHOOSE_RANDOM_LEADER = True
         print("sending add message to leader: ", self.leader) #debug
@@ -107,8 +105,6 @@ class Client:
         self.addmsgId = self.msgId
         instruction.messageId = self.addmsgId
         ALL_SENT_MESSAGES[self.msgId] = instruction
-        #print("message", ALL_SENT_MESSAGES[self.msgId].messageId, "stored in dictionary at index", self.msgId) #debug
-        #await instruction.handle(self) #debug
         await self._conn.send_message_to_server(instruction, self.leader)
           
     def get_reply_handler(self, msg):
@@ -146,7 +142,6 @@ class Client:
             yield line.decode()
 
     async def inputInstruction(self):
-        #print("in input instruction") # debug
         async for line in Client.generator(self, self._loop, sys.stdin):
             input = line.split()
             if len(input) <= 0:
@@ -156,21 +151,18 @@ class Client:
                     print("bad instruction")
                 else:
                     msg = Put(input[1], int(input[2]))
-                    #await msg.handle(self) #debug
                     self.sendQ.put(msg)
             elif input[0] == 'get':
                 if len(input) != 2:
                     print("bad instruction")
                 else: 
                     msg = Get(input[1])
-                    #await msg.handle(self) #debug
                     self.sendQ.put(msg)
             elif input[0] == 'add':
                 new_servers = []
                 for i in range (1, len(input)):
                     new_servers = new_servers + [int(input[i])]                    
                 msg = AddServers(set(new_servers))
-                #await msg.handle(self) #debug
                 self.addQ.put(msg)
             else: print("bad instruction")
 
@@ -182,11 +174,9 @@ class Client:
         global ADD_SUCCESS
         while True:
             msg = await self._conn.receive_message_from_server()
-            #await msg.handle(self) #debug
             sent_message = ALL_SENT_MESSAGES[msg.messageId]
             if msg.messageId == self.msgId and not msg.notleader:
                 if isinstance(msg, GetReply):
-                    #print("received reply to message ", msg.messageId) #debug
                     if  msg.success:
                         if msg.value is None:
                             print(sent_message.key, "-> <no value in store>") 
@@ -194,26 +184,18 @@ class Client:
                             print(sent_message.key, "->", msg.value)
                         SEND_SUCCESS = True 
                 if isinstance(msg, PutReply):
-                    #print("received reply to message ", msg.messageId) #debug
-                    #if not msg.success:
-                        #print("(",sent_message.key, ',', sent_message.value, ") not stored, retrying") #debug
                     if msg.success:
-                        #print("successfully stored (",sent_message.key, ',', sent_message.value, ")") #debug
-                        SEND_SUCCESS = True # we retry until key-value pair is stored
+                        SEND_SUCCESS = True 
             if isinstance(msg, AddServersReply) and msg.messageId == self.addmsgId and not msg.notleader:
-                print("received reply to message ", msg.messageId) #debug
-                #if not msg.success:
-                    #print("servers not added, retrying")
                 if msg.success:
-                    print("servers successfully added")
-                    ADD_SUCCESS = True # we retry until servers are added
+                    print("servers successfully added") #debug
+                    self.config.INIT_SERVER_CONFIG = self.config.INIT_SERVER_CONFIG.union(sent_message.servers)
+                    print("new config:", self.config.INIT_SERVER_CONFIG)
+                    ADD_SUCCESS = True 
             if (msg.messageId == self.msgId or msg.messageId == self.addmsgId) and msg.notleader and (not SEND_SUCCESS or not ADD_SUCCESS):
-                #print("responder not leader") #debug
                 if msg.leaderId is None:
-                    #print("msg.leaderId is not known") #debug
                     CHOOSE_RANDOM_LEADER = True
                 else:
-                    #print("msg.leaderId is known") #debug
                     CHOOSE_RANDOM_LEADER = False
                     self.leader = msg.leaderId
 
@@ -227,7 +209,6 @@ class Client:
                 self._loop.run_forever()
         except (KeyboardInterrupt, SystemExit):
             print()
-            #print("Client", self._id, "crashes")
         finally:
             pending = [t for t in asyncio.Task.all_tasks()]
             for t in pending:
